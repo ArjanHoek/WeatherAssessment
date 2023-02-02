1c76535ea79701cfd97cb759ddfd2578834a028f
Arjan Hoek <arjanhoek5191@gmail.com>
2023-02-02 21:36:57 +0100

    create basic components and setup basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { WeatherForecastComponent } from './weather-forecast/weather-forecast.component';
+import { WeatherComparisonComponent } from './weather-comparison/weather-comparison.component';
+import { RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+const routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'current-weather', component: CurrentWeatherComponent },
+  { path: 'weather-forecast', component: WeatherForecastComponent },
+  { path: 'weather-comparison', component: WeatherComparisonComponent },
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+  ],
+  declarations: [
+    AppComponent,
+    CurrentWeatherComponent,
+    WeatherForecastComponent,
+    WeatherComparisonComponent,
+    LandingPageComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/weather-comparison/weather-comparison.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-comparison',
+  templateUrl: './weather-comparison.component.html',
+  styleUrls: ['./weather-comparison.component.css']
+})
+export class WeatherComparisonComponent {
+
+}
