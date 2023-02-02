849076560ab04557ac2a84e41a14890428cacc05
Arjan Hoek <arjanhoek5191@gmail.com>
2023-02-02 22:05:10 +0100

    add basic api call and show data on screen

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherApiService } from 'src/services/weather-api.service';
 
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.css']
+  styleUrls: ['./current-weather.component.css'],
 })
-export class CurrentWeatherComponent {
+export class CurrentWeatherComponent implements OnInit {
+  currentWeatherData: {
+    temperature: number;
+  };
 
+  constructor(private weatherApiService: WeatherApiService) {}
+
+  ngOnInit(): void {
+    this.updateCurrentWeather();
+  }
+
+  updateCurrentWeather() {
+    this.weatherApiService.getCurrentWeather('Nijmegen').subscribe({
+      next: data => {
+        const {
+          current: { temp_c: temperature },
+        } = data;
+
+        this.currentWeatherData = { ...this.currentWeatherData, temperature };
+      },
+      error: error => {
+        if (error.status === 400) {
+          console.log('You might have entered some invalid input...');
+        }
+      },
+    });
+  }
 }
