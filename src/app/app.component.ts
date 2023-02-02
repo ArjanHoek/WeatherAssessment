e6d4714d31bfc1ce6c5078c5f6cc645a12f42c78
Arjan Hoek <arjanhoek5191@gmail.com>
2023-02-02 21:21:52 +0100

    inject weather service in main app component and add button to log current weather to console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component, VERSION } from '@angular/core';
+import { WeatherApiService } from 'src/services/weather-api.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  constructor(private weatherApiService: WeatherApiService) {}
 
+  logWeather() {
+    const weather = this.weatherApiService.getCurrentWeather('Nijmegen');
+    weather.subscribe(data => {
+      console.log(data);
+    });
+  }
 }
